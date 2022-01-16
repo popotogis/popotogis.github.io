09961f530fc39e72346c52d4a29ee7d1b320de16
TNakano-p <tomosuke.nakano@gmail.com>
2022-01-16 17:46:52 +0900

    popotogisロゴ追加

--- self-intro/src/App.vue
+++ self-intro/src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/img/popotogis.jpg">
+  <Profile/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Profile from './components/Profile.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Profile
   }
 });
 </script>
@@ -20,7 +20,7 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
